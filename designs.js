0a6bfb85808c7f5b65dbe201fc99cd87c4bc8f81
Daniel Otieno <danielotieno.ke@gmail.com>
2018-06-25 11:53:50 +0300

    created table grid for canvas

--- designs.js
+++ designs.js
@@ -7,6 +7,26 @@ function makeGrid() {
 
     let height = document.getElementById('inputHeight').value;
     let width = document.getElementById('inputWeight').value;
-    let table = document.getElementById('pixelCanvas')
+    let table = document.getElementById('pixelCanvas');
+
+    table.innerHTML = '';
+
+    let tbody = document.createElement('tbody');
+
+    for (let i = 0; i < height; i++) {
+
+        let tr = document.createElement('tr');
+
+        for (let j = 0; j < width; j++) {
+
+            let td = document.createElement('td');
+            td.appendChild(document.createTextNode(''));
+            tr.appendChild(td);
+        }
+
+        tbody.appendChild(tr);
+    }
+
+    table.appendChild(tbody);
 
 }
